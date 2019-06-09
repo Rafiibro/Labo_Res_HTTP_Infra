d4cbbe2d287ec81884c71643ba3c4abe6ac66176
rafiibro <rafael.dacunhagarcia@icloud.com>
2019-06-09 13:05:13 +0200

    Fin de la partie 2

--- docker-images/express-image/script.sh
+++ docker-images/express-image/script.sh
@@ -2,11 +2,11 @@
 # Kill all containers and apache_php image
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker rmi res/apache_php
+docker rmi res/express_students
 
 # Build the Docker image locally
-docker build -t res/apache_php .
+docker build -t res/express_students .
 
 clear
 
-docker run -d -p 9090:80 res/apache_php
+docker run -p 9090:3000 res/express_students
